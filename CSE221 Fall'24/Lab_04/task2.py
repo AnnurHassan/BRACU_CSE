77edf6d86ae904f98d73488096233c2f680eb1a1
annurhassan <annurhassan2002@gmail.com>
2025-01-04 03:36:22 +0600

    task2 modified

--- CSE221 Fall'24/Lab_04/task2.py
+++ CSE221 Fall'24/Lab_04/task2.py
@@ -14,30 +14,30 @@ with open("input2.txt", "r") as input:
     
     # utilities.print_dict(dict)
 
-    dict = {}
+    graph = {}
 
     for i in range(n+1):
-        dict[i] = []
+        graph[i] = []
     
     for i in range(m):
         arr = [int(i) for i in input.readline().strip().split()]
         u, v = arr
-        dict[u].append((v))
+        graph[u].append((v))
 
-    # utilities.print_dict(dict)
+    # utilities.print_dict(graph)
 
-    def BFS(dict, source):
+    def BFS(graph, source):
         seen_list = [source]
         q = deque()
         q.append(source)
 
         while q:
             u = q.popleft()
-            for i in dict[u]:
+            for i in graph[u]:
                 if i not in seen_list:
                     seen_list.append(i)
                     q.append(i)
         
         print(seen_list)
 
-    BFS(dict, 1)
+    BFS(graph, 1)
