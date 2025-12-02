f0341b83d4c5cac2c325f42dd88eae20d6a5560b
annurhassan <annurhassan2002@gmail.com>
2025-12-03 00:04:21 +0600

    added comments to lecture 1

--- CSE321_Fall_25/System_Call/lecture1.c
+++ CSE321_Fall_25/System_Call/lecture1.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
-// #include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
+#include <stdio.h>  
+#include <unistd.h>    // needed for fork()
+#include <stdlib.h>    // needed for exit() call 
+#include <sys/wait.h>  // needed for wait() call
 
 
 int main() {
     
-    int a = fork();
+    int a = fork();                                    // create a child process
 
-    if (a == 0) {
+    if (a == 0) {                                      // for child process
+         
+        printf("Child Process ID: %d\n", getpid());    // print the child Process ID
         
-        printf("Child Process ID: %d\n", getpid());
-        
-        // exit(0);
+        // exit(0);                                    // Not necessary for this instant but can be used to exit a process 
     }
-    else {
+    else {                                             // for parent process
         
-        // wait(NULL);
-        printf("Parent Process ID: %d\n", getpid());
+        wait(NULL);                                    // wait for the child process to finish first (if no wait child will be a zombie process)
+        printf("Parent Process ID: %d\n", getpid());   // print the parent process ID
         
-        // int n = wait(NULL);
+        // int n = wait(NULL);                         // wait also returns the process ID of child process
         // printf("Child Process returned: %d\n", n);
+        
         // exit(0);
     }
 
