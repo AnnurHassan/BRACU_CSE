f0ea2431761ba02347efd0e1d0a09a95764ccc6d
annurhassan <annurhassan2002@gmail.com>
2025-12-08 01:18:45 +0600

    part3 Modified

--- CSE321_Fall_25/System_Call/part3.c
+++ CSE321_Fall_25/System_Call/part3.c
@@ -1,3 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+int main() {
+
+    int fd = open("input.txt", O_WRONLY | O_APPEND);
+    char buffer[100];
+
+    int bytes = read(fd, buffer, sizeof(buffer));
+
+    printf("%s\n", buffer);
+    // write(STDOUT_FILENO, buffer, bytes);
+
+    char text[] = "Hello World from Write\n";
+
+    write(fd, text, sizeof(text));
+
+     close(fd);
+
+}
