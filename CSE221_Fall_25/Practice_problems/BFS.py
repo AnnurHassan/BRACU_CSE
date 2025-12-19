d1004123d7ee0fd4897773bdb6b6f6c2758855b2
annurhassan <annurhassan2002@gmail.com>
2025-12-19 11:34:12 +0600

    BFS Implemented in different ways

--- CSE221_Fall_25/Practice_problems/BFS.py
+++ CSE221_Fall_25/Practice_problems/BFS.py
@@ -1,16 +1,118 @@
-graph = {'A' : ['B', 'C'], 'B' : ['D', 'E', 'F'], 'C' : ['G'], 'D' : [], 'E' : [], 'F' : ['H'], 'G' : ['I'], 'H' : [], 'I' : []}
+from collections import deque
 
-def BFS(graph, start):
-    visited = [start]
-    queue = [start]
+
+graph = {
+        'r' : ['v', 's'], 
+        's' : ['w', 'r'], 
+        't' : ['w', 'x', 'u'], 
+        'u' : ['t', 'y'], 
+        'v' : ['r'], 
+        'w' : ['s', 't', 'x'], 
+        'x' : ['w', 't', 'y'], 
+        'y' : ['u', 'x'] 
+}
+
+
+""" BFS for just graph traversal  """
+
+# def BFS(graph, start):
+#     visited = [start]
+#     queue = deque([start])
+
+#     while queue:
+#         s = queue.popleft()
+#         print(s)
+
+#         for i in graph[s]:
+#             if i not in visited:
+#                 visited.append(i)
+#                 queue.append(i)
+
+
+"""  BFS for graph traversal and find out the shortedpath from every vertex to every vertex  """
+
+# def BFS(graph, start):
+    
+#     visited = {}
+#     for i in graph:
+#         visited[i] = 0
+    
+#     distance = {}
+#     for i in graph:
+#         distance[i] = 0
+    
+#     parent = {}
+#     for i in graph:
+#         parent[i] = 0
+    
+#     visited[start] = 1
+#     distance[start] = 0
+#     parent[start] = -1
+#     queue = deque([start])
+
+#     while queue:
+#         vertex = queue.popleft()
+#         print(vertex)
+
+#         for edge in graph[vertex]:
+#             if visited[edge] == 0:
+#                 visited[edge] = 1
+#                 distance[edge] = distance[vertex] + 1
+#                 parent[edge] = vertex
+#                 queue.append(edge)
+
+#     print(visited)
+#     print(distance)
+#     print(parent)
+
+
+"""  BFS for shortest path from a specifc source to destination  """
+
+def BFS(graph, start, destination):
+    
+    visited = {}
+    for i in graph:
+        visited[i] = 0
+    
+    parent = {}
+    for i in graph:
+        parent[i] = 0
+    
+    visited[start] = 1
+    parent[start] = -1
+    queue = deque([start])
 
     while queue:
-        s = queue.pop(0)
-        print(s)
+        vertex = queue.popleft()
 
-        for i in graph[s]:
-            if i not in visited:
-                visited.append(i)
-                queue.append(i)
+        for edge in graph[vertex]:
+            if visited[edge] == 0:
+                visited[edge] = 1
+                parent[edge] = vertex
 
-BFS(graph, 'A')
+                if edge == destination:
+                    path = compile_path(start, destination, visited, parent)
+                    print(f"{start}", end = " --> ")
+                    for i in path:
+                        if i == destination:
+                            print(i)
+                        
+                        else:
+                            print(f"{i}", end = " --> ")
+
+                    return 
+                queue.append(edge)
+
+
+def compile_path(start, destination, visited, parent):
+    path = []
+    while parent[destination] != -1:
+        path.append(destination)
+        destination = parent[destination]
+    
+    return path[::-1]
+
+
+BFS(graph, 's', 'u')
+
+
